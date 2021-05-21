9cc03512f9e9b975b5d70f0ed4f9edb2d0351eb1
Bernardo Margulis <bernardo@thismakesmehappy.co>
2021-05-21 16:06:47 -0400

    Colorize grid from image capture

--- final_project.py
+++ final_project.py
@@ -3,6 +3,7 @@ import random
 import cv2
 import numpy as np
 from colorsys import rgb_to_hsv, hsv_to_rgb
+from colorutils import hsv_to_hex, rgb_to_hex
 
 class Example(tk.Frame):
 
@@ -13,6 +14,8 @@ class Example(tk.Frame):
         self.columns = columns
         self.rows = rows
         self.spacing = spacing
+        self.current_image = np.ndarray(shape=(rows, columns))
+
 
         self.initUI()
 
@@ -24,19 +27,39 @@ class Example(tk.Frame):
         canvas = tk.Canvas(self)
         width = (self.canvas_width // self.columns) - self.spacing
         height = (self.canvas_height // self.rows) - self.spacing
-        for row in range (self.rows):
-            for column in range(self.columns):
-                # Calculate initial position for each shape
-                origin_y = row * (self.canvas_height / self.rows) + self.spacing // 2
-                origin_x = column * (self.canvas_width / self.columns) + self.spacing // 2
-                # Calculate middle points in each direction
-                middle_x = origin_x + (width // 2)
-                middle_y = origin_y + (height // 2)
 
-                self.draw_random_shape(canvas, width, height, origin_y, origin_x, middle_x, middle_y)
-
+        cap = cv2.VideoCapture(0)
+        ret, frame = cap.read()
+        #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        #gray = cv2.resize(gray, (640, 360), interpolation = cv2.INTER_AREA)
+        #frame[0, 0, 0] = 255
+        #frame[0,0, 1]=0
+        #frame[0,0, 2]=0
+        #color = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        #color = cv2.resize(color, (columns, rows), interpolation = cv2.INTER_AREA)
+        #color = cv2.cvtColor(color, cv2.COLOR_HSV2BGR)
+        frame = cv2.resize(frame, (self.columns, self.rows), interpolation=cv2.INTER_AREA)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        
+        while True:
+            
+            for row in range (self.rows):
+                for column in range(self.columns):
+                    color = rgb_to_hex((frame[row, column, :]))
+                    origin_y, origin_x, middle_x, middle_y = self.calculate_image_size_and_place(width, height, row, column)
+                    self.draw_random_shape(canvas, width, height, origin_y, origin_x, middle_x, middle_y, fill_color=color)
+            break
 
         canvas.pack(fill=tk.BOTH, expand=1)
+
+    def calculate_image_size_and_place(self, width, height, row, column):
+        # Calculate initial position for each shape
+        origin_y = row * (self.canvas_height / self.rows) + self.spacing // 2
+        origin_x = column * (self.canvas_width / self.columns) + self.spacing // 2
+        # Calculate middle points in each direction
+        middle_x = origin_x + (width // 2)
+        middle_y = origin_y + (height // 2)
+        return origin_y,origin_x,middle_x,middle_y
 
     def draw_random_shape(self, canvas, width, height, origin_y, origin_x, middle_x, middle_y, fill_color='#fff', outline_color='#000'):
         random_shape = random.randint(0, 6)
@@ -86,18 +109,8 @@ def main():
     ex = Example(canvas_height, canvas_width, columns, rows, spacing)
     geo = f'{canvas_width}x{canvas_height}+{canvas_origin_x}+{canvas_origin_y}'
     root.geometry(geo)
-    cap = cv2.VideoCapture(0)
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.resize(gray, (640, 360), interpolation = cv2.INTER_AREA)
-    dst = cv2.edgePreservingFilter(frame, flags=1, sigma_s=60, sigma_r=0.4)
-    color = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
 
-    print('got here')
-    color = cv2.resize(color, (10, 10), interpolation = cv2.INTER_AREA)
-    print(color)
-    cv2.imshow('frame',color)
-    print(color[0,0,0], color[0, 0, 1], color[0, 0, 2])
     root.mainloop()
 
 
