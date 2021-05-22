dedd4f3af68b1cb64217a09619a4df053e9f3f24
Bernardo Margulis <bernardo@thismakesmehappy.co>
2021-05-22 15:07:47 -0400

    Determine resize parameters

--- final_project.py
+++ final_project.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 from colorsys import rgb_to_hsv, hsv_to_rgb
 from colorutils import hsv_to_hex, rgb_to_hex
+import inspect
 
 class Example(tk.Frame):
 
@@ -14,7 +15,15 @@ class Example(tk.Frame):
         self.columns = columns
         self.rows = rows
         self.spacing = spacing
+        self.shape_width = (self.canvas_width // self.columns) - self.spacing
+        self.shape_height = (self.canvas_height // self.rows) - self.spacing
         self.current_image = np.ndarray(shape=(rows, columns))
+        self.resize_height = 0
+        self.resize_width = 0
+        cap = cv2.VideoCapture(0)
+        self.capture_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.capture_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.calculate_resize_parameters()
 
 
         self.initUI()
@@ -25,11 +34,12 @@ class Example(tk.Frame):
         self.pack(fill=tk.BOTH, expand=1)
 
         canvas = tk.Canvas(self)
-        width = (self.canvas_width // self.columns) - self.spacing
-        height = (self.canvas_height // self.rows) - self.spacing
+
 
         cap = cv2.VideoCapture(0)
         ret, frame = cap.read()
+
+        
         #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         #gray = cv2.resize(gray, (640, 360), interpolation = cv2.INTER_AREA)
         #frame[0, 0, 0] = 255
@@ -40,17 +50,26 @@ class Example(tk.Frame):
         #color = cv2.cvtColor(color, cv2.COLOR_HSV2BGR)
         frame = cv2.resize(frame, (self.columns, self.rows), interpolation=cv2.INTER_AREA)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        
         while True:
             
             for row in range (self.rows):
                 for column in range(self.columns):
                     color = rgb_to_hex((frame[row, column, :]))
-                    origin_y, origin_x, middle_x, middle_y = self.calculate_image_size_and_place(width, height, row, column)
-                    self.draw_random_shape(canvas, width, height, origin_y, origin_x, middle_x, middle_y, fill_color=color)
+                    origin_y, origin_x, middle_x, middle_y = self.calculate_image_size_and_place(self.shape_width, self.shape_height, row, column)
+                    self.draw_random_shape(canvas, self.shape_width, self.shape_height, origin_y, origin_x, middle_x, middle_y, fill_color=color)
             break
 
         canvas.pack(fill=tk.BOTH, expand=1)
+
+    def calculate_resize_parameters(self):
+        ''' Given the size of the captured image, determine the width and the height of the resized image, and how to crop it, to mantain proportions with the rows and columns of the pixel art '''
+        width_resized = (self.rows * self.capture_width) // self.capture_height
+        if width_resized > self.columns:
+            self.resize_height = self.rows
+            self.resize_width = width_resized
+        else:
+            self.resize_height = (self.columns * self.capture_height) // self.capture_width
+            self.resize_width = self.columns
 
     def calculate_image_size_and_place(self, width, height, row, column):
         # Calculate initial position for each shape
@@ -63,7 +82,7 @@ class Example(tk.Frame):
 
     def draw_random_shape(self, canvas, width, height, origin_y, origin_x, middle_x, middle_y, fill_color='#fff', outline_color='#000'):
         random_shape = random.randint(0, 6)
-        border_width = 2
+        border_width = 1
         shrink_factor = 3
         small_width = width // shrink_factor
         small_height = height // shrink_factor
@@ -98,18 +117,17 @@ class Example(tk.Frame):
 def main():
 
     # TODO: Account for wdith of window frame
-    canvas_width = 600
-    canvas_height = 400
+    canvas_width = 800
+    canvas_height = 600
     canvas_origin_x = 00
     canvas_origin_y = 00
-    columns = 21
-    rows = 15
-    spacing = 8
+    columns = 88
+    rows = 41
+    spacing = 2
     root = tk.Tk()
     ex = Example(canvas_height, canvas_width, columns, rows, spacing)
     geo = f'{canvas_width}x{canvas_height}+{canvas_origin_x}+{canvas_origin_y}'
     root.geometry(geo)
-    
 
     root.mainloop()
 
---
+++ steps.md
@@ -0,0 +1,27 @@
+- create window
+- calculate sizes for columns and rows
+-capture initial image
+determine resize parameters
+    determine proportions of columns to row
+    determine proportions of image width to image height
+    determine the direction to resize over
+    determine how the image will need to be cropped
+loop
+    capture camera image
+    convert to hsv
+    resize image
+    crop image as necessary
+    do any necessary image manipulations
+    draw the grid
+        extract each color from the image
+        convert the color to hex
+        draw the corresponding shape with the color picked
+    loop camera capture
+        downsample (smaller image, and grayscale)
+        calculate the dense flow
+        if the dense flow is above the threshold
+            signal 3 seconds to give the prson time
+                lighten the shapes to 75%, then 50%, then 25%, one second each
+            exit loop
+
+    
